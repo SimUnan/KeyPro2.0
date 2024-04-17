157c67f2f699ab4ac72833cb8bed71e869aba0d3
Sim Unan <simunan002@gmail.com>
2024-04-17 12:07:14 +0700

    first dev push

--- app/product/section/Product.tsx
+++ app/product/section/Product.tsx
@@ -3,15 +3,9 @@ import ProductComponent from '../component/ProductComponent'
 
 const Product = () => {
   return (
-    <section className='w-full h-auto py-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8'>
+    <section className='w-full bg-red-200 h-auto py-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8'>
         <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
-        <ProductComponent />
+        
     </section>
   )
 }
