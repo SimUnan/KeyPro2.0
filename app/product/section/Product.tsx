a58a9f69b44cf728a50c7827c5a206d516a0b6c3
Sim Unan <simunan002@gmail.com>
2024-04-17 12:08:00 +0700

    dev commit

--- app/product/section/Product.tsx
+++ app/product/section/Product.tsx
@@ -5,7 +5,7 @@ const Product = () => {
   return (
     <section className='w-full bg-red-200 h-auto py-10 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8'>
         <ProductComponent />
-        
+        <p>HI</p>
     </section>
   )
 }
