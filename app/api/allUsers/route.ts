6488e199b5a49ab7bcac7b0023cfe332f6226039
Unan <simunan002@gmail.com>
2024-08-06 10:26:43 +0700

    fix build

--- app/api/allUsers/route.ts
+++ app/api/allUsers/route.ts
@@ -2,7 +2,7 @@ import prisma from '@/libs/prismadb'
 import { error } from 'console';
 import { NextResponse } from 'next/server'
 
-export async function GET(res: Response){
+export async function GET(req: Request){
     
     const allUsers = await prisma.user.findMany();
     
